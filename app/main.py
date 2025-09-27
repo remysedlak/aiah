cd7f7d23792695bdf2812ea7ac6a6c5dcdcab058
remysedlak <remysedlak@gmail.com>
2025-09-27 18:04:45 -0400

    add post response

--- app/main.py
+++ app/main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+# Define the request body model
+class Message(BaseModel):
+    text: str
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.post("/")
+async def echo(msg: Message):
+    return {"you_said": msg.text}
